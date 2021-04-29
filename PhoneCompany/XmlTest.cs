d3e585946dece67eba260798eb391ab4bc26981e
oliverlowbob <oliverlowbob@live.dk>
2021-04-29 12:52:06 +0200

    adds more test methods

--- PhoneCompany/XmlTest.cs
+++ PhoneCompany/XmlTest.cs
@@ -35,5 +35,27 @@ namespace PhoneCompany
 
             Assert.AreEqual(element.Item(0).InnerText, deliveryInfo.DeliveryId);
         }
+
+        [Test]
+        public void TestDeliveryInfoCustomer()
+        {
+            XmlDocument deliveryXml = new XmlDocument();
+
+            Serializer ser = new Serializer();
+
+            var workingDirectory = Environment.CurrentDirectory;
+
+            var path = Directory.GetParent(workingDirectory).Parent.Parent.FullName + @"\Delivery.xml";
+
+            deliveryXml.Load(path);
+
+            var xmlInputData = File.ReadAllText(path);
+
+            DeliveryInfo deliveryInfo = ser.Deserialize<DeliveryInfo>(xmlInputData);
+
+            var element = deliveryXml.GetElementsByTagName("Customer");
+
+            Assert.AreEqual(element.Item(0).FirstChild.InnerText, deliveryInfo.Customer.Name);
+        }
     }
 }
